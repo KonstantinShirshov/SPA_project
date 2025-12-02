148ecc840c2b5fc0c6d4958d49f54e33caca4f01
Konstantin <shirshikk@gmail.com>
2025-12-02 20:34:58 +0500

    finish commit

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("courses/", include("materials.urls", namespace="courses")),
+    path("payments/", include("users.urls", namespace="payments")),
 ]
